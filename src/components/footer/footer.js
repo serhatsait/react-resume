1bb9f9836aee8c609119f11b32e0ec6bbf8814fb
serhatsait <serhatsaitpekediz@gmail.com>
2020-10-30 17:25:13 +0300

    minor

---
+++ src/components/footer/footer.js
@@ -0,0 +1,37 @@
+import React, {Fragment} from 'react'
+
+function Footer() {
+  return (
+      <Fragment>
+        <footer className="footer">
+          <div className="container text-center">
+            <a className="cc-facebook btn btn-link" href="#">
+              <i className="fa fa-facebook fa-2x " aria-hidden="true"></i>
+            </a>
+            <a className="cc-twitter btn btn-link " href="#">
+              <i className="fa fa-twitter fa-2x " aria-hidden="true"></i>
+            </a>
+            <a className="cc-google-plus btn btn-link" href="#">
+              <i className="fa fa-google-plus fa-2x" aria-hidden="true"></i>
+            </a>
+            <a className="cc-instagram btn btn-link" href="#">
+              <i className="fa fa-instagram fa-2x " aria-hidden="true"></i>
+            </a>
+          </div>
+          <div className="h4 title text-center">Anthony Barnett</div>
+          <div className="text-center text-muted">
+            <p>
+              &copy; Creative CV. All rights reserved.
+              <br />
+              Design -{' '}
+              <a className="credit" href="https://templateflip.com" target="_blank">
+                TemplateFlip
+              </a>
+            </p>
+          </div>
+        </footer>
+      </Fragment>
+  )
+}
+
+export default Footer
